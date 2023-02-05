3c10d2d15096464704fa7176ffcc5512d9612428
md.amanullah@technovista.com.bd <md.amanullah@technovista.com.bd>
2023-02-05 10:37:42 +0600

    Update AuditService.cs

--- AuditCenter/Services/AuditService.cs
+++ AuditCenter/Services/AuditService.cs
@@ -1,33 +1,34 @@
-﻿using System.Globalization;
+using System.Globalization;
 using Grpc.Core;
 using Northwind.IntegrationEvents.Protobuf.Audit.V1;
 
-namespace AuditCenter.Services;
-
-public class AuditService : Auditor.AuditorBase
+namespace AuditCenter.Services
 {
-    private readonly ILogger<AuditService> _logger;
-
-    public AuditService(ILogger<AuditService> logger)
+    public class AuditService : Auditor.AuditorBase
     {
-        _logger = logger;
-    }
+        private readonly ILogger<AuditService> _logger;
 
-    public override async Task<SubmitAuditResponse> SubmitAudit(SubmitAuditRequest request, ServerCallContext context)
-    {
-        if (request.Event == "StateMachine.Started")
+        public AuditService(ILogger<AuditService> logger)
         {
-            _logger.Log(LogLevel.Information, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            _logger = logger;
         }
 
-        _logger.Log(LogLevel.Information,
-            "[{Event}]-[{AuditedAt}] Actor is [{Actor}] with Order is [{CorrelateId}] and Status is [{Status}]",
-            request.Event,
-            request.AuditedAt.ToDateTime().ToString(CultureInfo.InvariantCulture),
-            request.Actor,
-            request.CorrelateId,
-            request.Status);
+        public override async Task<SubmitAuditResponse> SubmitAudit(SubmitAuditRequest request, ServerCallContext context)
+        {
+            if (request.Event == "StateMachine.Started")
+            {
+                _logger.Log(LogLevel.Information, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            }
 
-        return await Task.FromResult(new SubmitAuditResponse {Done = true});
+            _logger.Log(LogLevel.Information,
+                "[{Event}]-[{AuditedAt}] Actor is [{Actor}] with Order is [{CorrelateId}] and Status is [{Status}]",
+                request.Event,
+                request.AuditedAt.ToDateTime().ToString(CultureInfo.InvariantCulture),
+                request.Actor,
+                request.CorrelateId,
+                request.Status);
+
+            return await Task.FromResult(new SubmitAuditResponse { Done = true });
+        }
     }
 }
